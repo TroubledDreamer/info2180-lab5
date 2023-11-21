33a2e2327e50caff4f8e31413ba8c224a1261ac2
TroubledDreamer <carlyonja@gmail.com>
2023-11-20 20:40:43 -0500

    Table implemented

--- world.php
+++ world.php
@@ -14,6 +14,29 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+
+<table>
+  <thead>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence</th>
+    <th>Head of state</th>
+  </thead>
+  <tbody>
+    <?php foreach ($results as $result): ?>
+    <tr>
+      <td><?= $result['name']; ?></td>
+      <td><?= $result['continent']; ?></td>
+      <td><?= $result['independence_year']; ?></td>
+      <td><?= $result['head_of_state']; ?></td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+
+
+
+
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
